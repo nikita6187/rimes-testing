7a9b29c7f69baaf0b91c3785ffbb8d801eacfb86
nikita68 <n.makarov@hotmail.com>
2018-06-12 11:03:36 +0200

    Configs

--- run_rimes_training.sh
+++ run_rimes_training.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/lib64
 
 source ./ENV/bin/activate
 
-python3 ../returnn/rnn.py ./returnn_tests/tf-neural-transducer.rimes.200-20.2048.config
+python3 ../returnn/rnn.py ./returnn_tests/tf-neural-transducer.rimes.50-8.2048.config
 
 
