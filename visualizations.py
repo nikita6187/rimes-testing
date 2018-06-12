b4d84b14cfbff7b38acb425782bdd182f4a537a7
nikita68 <n.makarov@hotmail.com>
2018-06-12 09:36:32 +0200

    Graphics!

--- visualizations.py
+++ visualizations.py
@@ -20,7 +20,10 @@ def main():
             for line in f:
                 split = line.split(' ')
                 if len(split) > 6:
-                    if re.match("^\d+?\.\d+?$", split[6].split(',')[0]) is not None:
+                    if len(split) >= 10 and "'dev'" in split[2] \
+                            and re.match("^\d+?\.\d+?$", split[9].split(',')[0]) is not None:
+                        data1.append(float(split[9].split(',')[0]))
+                    elif re.match("^\d+?\.\d+?$", split[6].split(',')[0]) is not None:
                         data1.append(float(split[6].split(',')[0]))
                 elif len(split) == 2:
                     split2 = re.split(r'\[|\]', split[1])
